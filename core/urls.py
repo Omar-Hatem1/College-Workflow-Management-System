274138fc5c6303bb8bbf489e62ea9249a1e6b693
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-15 16:06:06 +0200

    clean core

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from django.urls import path
 from core.views import *
-
-#router = SimpleRouter() 
-# router.register('users', UsersViewSet)
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    #path('', include(router.urls)),
-    # path ('users/', UsersViewSet.as_view()),
-    # path ('users/<int:pk>', UsersViewSet.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListAPIView
-from core.models import User
-from core.serializers import UserSerializer , TokenObtainPairSerializer
+from core.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView as BaseTokenObtainPairView
 
-# class UsersViewSet (ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-    
 class TokenObtainPairView (BaseTokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
