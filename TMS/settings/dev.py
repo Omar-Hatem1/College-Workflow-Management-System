52df11485edff5630e2ac8fff17eb0bfe20edde9
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-16 03:36:44 +0200

    update allowed hosts

--- TMS/settings/dev.py
+++ TMS/settings/dev.py
@@ -2,7 +2,7 @@ from .common import *
 
 SECRET_KEY = 'django-insecure-ki2j_&zb!dq%0d90ezdj^b(b57qsmpp40+oomc97dxo+4s63l-'
 
-ALLOWED_HOSTS = ['web-production-c1b7.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 DEBUG = True
 
 # DATABASES = {
@@ -22,5 +22,5 @@ DATABASES = {
     }
 }
 CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-c1b7.up.railway.app'
+    '*'
 ]
