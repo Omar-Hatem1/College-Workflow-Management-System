ee7ed109edff643fd40d5be3b6c14829df615a72
Omar-Hatem1 <gamemanager617@gmail.com>
2023-05-21 02:27:31 +0300

    first commit

--- TMS/settings/prod.py
+++
@@ -1,9 +0,0 @@
-import os
-from .common import *
-
-SECRET_KEY = os.environ['SECRET_KEY']
-
-
-DEBUG = False
-
-#ALLOWED_HOSTS = ['web-production-c1b7.up.railway.app']
--- core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User (AbstractUser):
-    email=models.EmailField(max_length=255, unique=True) 
-
-    def __str__(self) -> str:
-        return self.first_name +" "+ self.last_name
