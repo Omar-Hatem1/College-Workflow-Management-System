b65704e36d450795a82a6ca3f878ab796fb2d942
Omar-Hatem1 <gamemanager617@gmail.com>
2023-06-17 02:20:54 +0300

    edits in permission

--- tasker/permissions.py
+++ tasker/permissions.py
@@ -17,13 +17,22 @@ class CanReceiveTask(BasePermission):
         return bool(request.user and request.user.staff.role != 'admin' and  request.user.staff.role != 'dean')
     
 class IsDoctorOrAssistant(BasePermission):
+    """
+    Allows access only to users who can send leaves request.
+    """
     def has_permission(self, request, view):
        return bool(request.user and (request.user.staff.role == 'doctor' or request.user.staff.role == 'assistant'))
 
 class IsDeanViceDeanHOD(BasePermission):
+    """
+    Allows access only to users who can accept/refuse leaves request.
+    """
     def has_permission(self, request, view):
         return bool(request.user and (request.user.staff.role == 'dean' or request.user.staff.role == 'vice' or request.user.staff.role == 'head'))
     
 class IsSecretaryAdmin(BasePermission):
+    """
+    Allow only Admin and Secretary to view Accepted Leaves
+    """
     def has_permission(self, request, view):
         return bool(request.user and (request.user.staff.role == 'secretary' or request.user.staff.role == 'admin'))
